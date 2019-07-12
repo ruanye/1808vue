ef2fba74666c99e69829cabece4e1e028a79d317
ruanye <55343581@qq.com>
2019-07-12 15:19:33 +0800

    vue打包配置和别名配置

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   // Vue-CLI 3.3以前publicPath叫做baseUrl
   // publicPath: 'www.baidu.com', // 指定域名
-  outputDir: 'build', // 打包出来的路径，默认叫做dist
+  // outputDir: 'build', // 打包出来的路径，默认叫做dist
   assetsDir: 'static', // 静态资源目录 js css img fonts 1常用
   productionSourceMap: false, // 生产soucemap文件  false 不生成  2常用
   // 修改webpack配置
